d924d5b412a101b7ec0b779569865b067aa7fd48
Ibrahim Sarker <168856336+isarker8@users.noreply.github.com>
2026-08-20 09:24:51 +0100

    initial dmenu build

---
+++ config.def.h
@@ -0,0 +1,36 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrainsMono Nerd Font:size=13"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+#include "/home/isarker/.cache/wal/colors-wal-dmenu.h"
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* X resources to load at startup */
+static const XResPref resources[] = {
+	/* name                  type     address */
+	{ "dmenu.font",          STRING,  &fonts[0] },
+	{ "dmenu.prompt",        STRING,  &prompt },
+	{ "dmenu.foreground",    STRING,  &colors[SchemeNorm][ColFg] },
+	{ "dmenu.background",    STRING,  &colors[SchemeNorm][ColBg] },
+	{ "dmenu.foregroundSel", STRING,  &colors[SchemeSel][ColFg] },
+	{ "dmenu.backgroundSel", STRING,  &colors[SchemeSel][ColBg] },
+	{ "dmenu.foregroundOut", STRING,  &colors[SchemeOut][ColFg] },
+	{ "dmenu.backgroundOut", STRING,  &colors[SchemeOut][ColBg] },
+	{ "dmenu.topbar",        INTEGER, &topbar },
+	{ "dmenu.lines",         INTEGER, &lines },
+};
